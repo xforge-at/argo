94e30e1ad09ee8d5cbd8e2951978d77382b907be
Michael Heinzl <m.heinzl@xforge.at>
2016-07-10 16:24:38 +0200

    added HttpRequestor Implementation for iOS

--- ios/ArgoApp/ArgoApp-Bridging-Header.h
+++ ios/ArgoApp/ArgoApp-Bridging-Header.h
@@ -2,6 +2,10 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import "XFHttpRequestor.h"
+#import "XFHttpRequestorCallback.h"
 #import "XFMyRecord.h"
 #import "XFMyRecordGetter.h"
 #import "XFMyRecordGetterDelegate.h"
+#import "XFRequest.h"
+#import "XFResponse.h"
---
+++ ios/ArgoApp/HTTPRequestor.swift
@@ -0,0 +1,38 @@
+//
+//  HTTPRequestor.swift
+//  ArgoApp
+//
+//  Created by Michael Heinzl on 10.07.16.
+//
+//
+
+import Foundation
+
+class HTTPRequestor: NSObject, XFHttpRequestor {
+
+	func executeRequest(request: XFRequest, callback: XFHttpRequestorCallback) {
+		let urlRequest = NSMutableURLRequest(URL: NSURL(string: request.url)!)
+		let session = NSURLSession.sharedSession()
+		urlRequest.HTTPMethod = request.method
+		urlRequest.HTTPBody = request.body
+
+		if let header = request.header {
+			for (k, v) in header {
+				urlRequest.addValue(v, forHTTPHeaderField: k)
+			}
+		}
+
+		session.dataTaskWithRequest(urlRequest, completionHandler: { data, response, error -> Void in
+			if let error = error {
+				let e = XFError.init(message: error.localizedDescription)
+				callback.receiveError(e)
+			} else if let httpResponse = response as? NSHTTPURLResponse {
+				let response = XFResponse.init(request: request, header: httpResponse.allHeaderFields as! [String: String], body: data)
+				callback.receiveResponse(response)
+			} else {
+				let e = XFError.init(message: "Something went wrong")
+				callback.receiveError(e)
+			}
+		}).resume()
+	}
+}
