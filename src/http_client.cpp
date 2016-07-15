8024736123c7cd0d04cfa3c548686c39b772cf0e
Michael Heinzl <m.heinzl@xforge.at>
2016-07-15 15:04:53 +0200

    added http_client

---
+++ src/http_client.cpp
@@ -0,0 +1,34 @@
+#import "generated/error.hpp"
+#import "generated/http_requestor.hpp"
+#import "generated/response.hpp"
+#import "http_client.hpp"
+
+HTTPClient::HTTPClient(nn_shared_ptr<HttpRequestor> &requestor) : requestor{requestor} {};
+
+void HTTPClient::get(const string &url, success_block_t success, error_block_t error) { this->send(HTTPMethod::GET, url, success, error); }
+
+void HTTPClient::send(HTTPMethod method, const string &url, success_block_t success, error_block_t error) {
+
+    nn_shared_ptr<Callback> cb = make_not_null(make_shared<Callback>(success, error));
+
+    // add session token
+
+    let request = RequestFactory::buildRequest(method, url);
+    this->requestor->execute_request(request, cb);
+}
+
+void HTTPClient::Callback::receive_response(const Argo::Response &response) {
+
+    // check http status code
+
+    string bodyString{response.body->begin(), response.body->end()};
+    string jsonError;
+    auto json_response = Json::parse(bodyString, jsonError);
+    if (!jsonError.empty()) {
+        this->error(Error{jsonError});
+    } else {
+        this->success(json_response);
+    }
+}
+
+void HTTPClient::Callback::receive_error(const Argo::Error &error) { this->error(error); }
---
+++ src/http_client.hpp
@@ -0,0 +1,39 @@
+#import "generated/http_requestor.hpp"
+#import "generated/http_requestor_callback.hpp"
+#import "request_factory.hpp"
+#import "util.hpp"
+#import <json11/json11.hpp>
+
+using std::function;
+using json11::Json;
+using Argo::HttpRequestor;
+using Argo::Request;
+using Argo::Response;
+using Argo::Error;
+
+typedef function<void(Json)> success_block_t;
+typedef function<void(Error)> error_block_t;
+
+class HTTPClient {
+  public:
+    virtual ~HTTPClient() {}
+    HTTPClient(nn_shared_ptr<HttpRequestor> &requestor);
+
+    void get(const string &url, success_block_t success, error_block_t error);
+
+  private:
+    nn_shared_ptr<HttpRequestor> requestor;
+
+    void send(HTTPMethod method, const string &url, success_block_t success, error_block_t error);
+
+    class Callback : public Argo::HttpRequestorCallback {
+      public:
+        Callback(success_block_t success, error_block_t error);
+        virtual void receive_response(const Response &response);
+        virtual void receive_error(const Error &error);
+
+      private:
+        success_block_t success;
+        error_block_t error;
+    };
+};
