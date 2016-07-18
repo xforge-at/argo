a7086e6932e2e19a6d993a8925016d6128aa5f89
Manu Wallner <m.wallner@xforge.at>
2016-07-18 16:08:56 +0200

    Compile empty main queue helper on iOS

--- src/main_thread_helper.cpp
+++ src/main_thread_helper.cpp
@@ -1,25 +1,35 @@
 #import "generated/main_thread_dispatch_queue_helper.hpp"
+
 #ifdef __ANDROID__
 
 #import <dispatch/dispatch.h>
 #import <sys/select.h>
+
+#endif
 
 using namespace Argo;
 
 class main_thread_helper : public MainThreadDispatchQueueHelper {
   public:
     main_thread_helper();
-    virtual ~main_thread_helper() override{};
+    virtual ~main_thread_helper() override{}
     void drain_queue() override;
     bool wait_for_items(int32_t timeout_microseconds) override;
 
   private:
+#ifdef __ANDROID__
     dispatch_queue_handle_t _handle;
+#endif
 };
 
-main_thread_helper::main_thread_helper() : _handle(dispatch_get_main_queue_handle_np()) {}
+main_thread_helper::main_thread_helper() {
+#ifdef __ANDROID__
+    _handle = dispatch_get_main_queue_handle_np();
+#endif
+}
 
 bool main_thread_helper::wait_for_items(int32_t timeout_microseconds) {
+#ifdef __ANDROID__
     fd_set readable_set;
     FD_ZERO(&readable_set);
     FD_SET(this->_handle, &readable_set);
@@ -29,14 +39,17 @@ bool main_thread_helper::wait_for_items(int32_t timeout_microseconds) {
     time.tv_usec = timeout_microseconds;
 
     return select(this->_handle + 1, &readable_set, NULL, NULL, timeout_microseconds != 0 ? &time : NULL) > 0;
+#else
+    return false;
+#endif
 }
 
 void main_thread_helper::drain_queue() {
+#ifdef __ANDROID__
     dispatch_main_queue_drain_np();
+#endif
 }
 
 nn_shared_ptr<MainThreadDispatchQueueHelper> Argo::MainThreadDispatchQueueHelper::create() {
     return make_not_null(make_shared<main_thread_helper>());
 }
-
-#endif
