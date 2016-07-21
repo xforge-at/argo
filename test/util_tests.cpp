e3f0a7c6510b88353210ddd053c15d76ff9e0f65
Manu Wallner <m.wallner@xforge.at>
2016-07-21 15:04:52 +0200

    Add unneccessary util test

--- test/util_tests.cpp
+++ test/util_tests.cpp
@@ -1,3 +1,10 @@
 #import "util.hpp"
 #import <gmock/gmock.h>
 #import <gtest/gtest.h>
+
+TEST(UtilTest, MakeNotNullShouldAddNotNullAnnotation) {
+    auto x = 5;
+    auto xPtr = make_not_null(&x);
+
+    ASSERT_EQ(x, *xPtr) << "X and the not null pointer should be equal";
+}
