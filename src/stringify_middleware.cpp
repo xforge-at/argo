898590f5b4f9d6c38dd37b93eab16b640120cbe8
Michael Heinzl <m.heinzl@xforge.at>
2016-07-26 16:34:36 +0200

    added stringily middleware + tests

---
+++ src/stringify_middleware.cpp
@@ -0,0 +1,9 @@
+#import "string_component.hpp"
+#import "stringify_middleware.hpp"
+
+MiddlewareResponse StringifyMiddleware::handle_response(Response &response) {
+    string bodyString{response.body->begin(), response.body->end()};
+    let comp = make_shared<StringComponent>(bodyString);
+    response.components.insert({"bodyString", comp});
+    return MiddlewareResponse{ftl::constructor<Argo::Response>(), response};
+}
