a1c02fe5646a1e534a77ce90bb7610b61d093a4a
Manu Wallner <m.wallner@xforge.at>
2016-08-01 16:07:21 +0200

    Fix mock imports

--- test/http_requestor_mock.hpp
+++ test/http_requestor_mock.hpp
@@ -1,17 +1,17 @@
-#import "util.hpp"
-#import "http_requestor.hpp"
-#import "generated/request.hpp"
+#import "extended/response.hpp"
 #import "generated/error.hpp"
-#import "response.hpp"
+#import "generated/request.hpp"
+#import "http_requestor.hpp"
 #import "http_requestor_callback.hpp"
+#import "util.hpp"
 
 class HTTPRequestorMock : public Argo::HttpRequestor {
-    public:
-        void setResponse(Argo::Response &response);
-        void setError(Argo::Error &error);
+  public:
+    void setResponse(Argo::Response &response);
+    void setError(Argo::Error &error);
 
-        void execute_request(const Argo::Request & request, const nn_shared_ptr<Argo::HttpRequestorCallback> & callback) override;
+    void execute_request(const Argo::Request &request, const nn_shared_ptr<Argo::HttpRequestorCallback> &callback) override;
 
-        Argo::Response *response;
-        Argo::Error *error;
+    Argo::Response *response;
+    Argo::Error *error;
 };
