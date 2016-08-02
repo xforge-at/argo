5eb611d923f5f3f6500e4db4e7a8656d77ea22e1
Manu Wallner <m.wallner@xforge.at>
2016-08-02 18:54:16 +0200

    Stub out handle_request method
    (implement later to add accept-encoding header)
    (also check content-encoding in handle_response)

--- src/jsonify_middleware.cpp
+++ src/jsonify_middleware.cpp
@@ -1,22 +1,24 @@
+#import "jsonify_middleware.hpp"
 #import "generated/error.hpp"
 #import "json_component.hpp"
-#import "jsonify_middleware.hpp"
 #import "string_component.hpp"
 
 MiddlewareResponse JsonifyMiddleware::handle_response(Response &response) {
     let tempString = response.get_component<StringComponent>("bodyString");
-    if (tempString) {
-        string jsonError;
-        auto jsonBody = Json::parse(*tempString, jsonError);
-        if (!jsonError.empty()) {
-            let error = Error{0, jsonError};
-            return break_response_chain(error);
-        } else {
-            let comp = make_shared<JsonComponent>(jsonBody);
-            response.components.insert({"bodyJson", comp});
-            return follow_chain(response);
-        }
+    if (!tempString) return follow_chain(response);
+
+    string jsonError;
+    auto jsonBody = Json::parse(*tempString, jsonError);
+    if (!jsonError.empty()) {
+        let error = Error{0, jsonError};
+        return break_response_chain(error);
     } else {
+        let comp = make_shared<JsonComponent>(jsonBody);
+        response.components.insert({"bodyJson", comp});
         return follow_chain(response);
     }
+}
+
+MiddlewareRequest JsonifyMiddleware::handle_request(Request &request) {
+    return follow_chain(request);
 }
