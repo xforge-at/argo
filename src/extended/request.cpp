978cce2e8923745f11a2265cb231a5f4f36b1a95
Manu Wallner <m.wallner@xforge.at>
2016-08-09 18:58:28 +0200

    Normalize header keys

--- src/extended/request.cpp
+++ src/extended/request.cpp
@@ -1,14 +1,23 @@
 #import "request.hpp"
+#import <algorithm>
+#import <string>
+
+using std::transform;
 
 void Argo::Request::add_header(string key, string value) {
     let h = this->header;
 
+    var normalized_key = string{key};
+    transform(key.begin(), key.end(), normalized_key.begin(), ::tolower);
+
     if (h) {
         var header = *h;
-        header.insert({key, value});
+        // Erase the old header before (if one exists)
+        header.erase(normalized_key);
+        header.insert({normalized_key, value});
         this->header = header;
     } else {
-        unordered_map<string, string> header{{key, value}};
+        unordered_map<string, string> header{{normalized_key, value}};
         this->header = make_optional(header);
     }
 }
--- test/request_tests.cpp
+++ test/request_tests.cpp
@@ -28,6 +28,17 @@ TEST(RequestTests, AddHeaderLeavesOldDictAlone) {
     ASSERT_EQ(headerinos.at("old_key"), "old_value") << "The old values that were passed during init should still exist";
 }
 
+TEST(RequestTests, AddHeaderIsCaseInsensitive) {
+    Request req{"GET", "www.test.at", nullopt, nullopt};
+
+    req.add_header("test_key", "test_value2");
+    req.add_header("tesT_Key", "test_value1");
+
+    ASSERT_TRUE(req.header) << "Headers should already exist";
+    let headerinos = *req.header;
+    ASSERT_EQ(headerinos.at("test_key"), "test_value1") << "The first key should have been overwritten";
+}
+
 TEST(RequestTests, AddHeaderCanAddMoreThanOneThing) {
     Request req{"GET", "www.test.at", nullopt, nullopt};
 
@@ -40,4 +51,14 @@ TEST(RequestTests, AddHeaderCanAddMoreThanOneThing) {
     ASSERT_EQ(headerinos.at("test_key1"), "test_value1") << "New values should have been inserted after adding the header";
     ASSERT_EQ(headerinos.at("test_key2"), "test_value2") << "New values should have been inserted after adding the header";
     ASSERT_EQ(headerinos.at("test_key3"), "test_value3") << "New values should have been inserted after adding the header";
+}
+
+TEST(RequestTests, AppendHeaderAddsHeaderIfNoneExists) {
+    Request req{"GET", "www.test.at", nullopt, nullopt};
+
+    req.append_to_header("Accept", "test1");
+
+    ASSERT_TRUE(req.header) << "New headers should have been created";
+    let headerinos = *req.header;
+    ASSERT_EQ(headerinos.at("test_key"), "test_value");
 }
