aa34e0305334203ed3e347717ea387d57e3cf57b
Manu Wallner <m.wallner@xforge.at>
2016-09-30 22:48:49 -0700

    Update device list

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 case "$BUILD_TARGET" in 
    "ios")
-      scan --project ios/ArgoLib.xcodeproj/ --devices "['iPhone 5', 'iPhone 6', 'iPhone 6+']" --scheme "Argo"
+      scan --project ios/ArgoLib.xcodeproj/ --devices "iPhone 5" "iPhone 6" "iPhone 6+" --scheme "Argo"
       ;;
    "android")
       ;;
