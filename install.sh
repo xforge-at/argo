6380d8b91ad90aaec19e7c5c9ef600d5f5c3c41c
Manu Wallner <m.wallner@xforge.at>
2016-09-30 23:07:31 -0700

    Do android ndk stuff

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ case "$BUILD_TARGET" in
       make ios
       ;;
    "android")
+      mkdir -p $ANDROID_NDK
+      wget https://dl.google.com/android/repository/android-ndk-r12b-linux-x86_64.zip -O ndk.zip
+      unzip ndk.zip -d $ANDROID_NDK
       make android
       ;;
    "cpp")
